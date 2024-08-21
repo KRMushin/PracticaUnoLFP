3bf5d57aa5a04aa1260a7b0d8b4bf5bf5caf2e30
KRMushin <kevinramirez202231207@cunoc.edu.gt>
2024-08-21 13:59:06 -0600

    Inicializando el repositorio de practica uno LFPSS2024

---
+++ PracticaUnoLFP/src/main/java/com/mycompany/practicaunolfp/PracticaUnoLFP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicaunolfp;
+
+/**
+ *
+ * @author kevin-mushin
+ */
+public class PracticaUnoLFP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
